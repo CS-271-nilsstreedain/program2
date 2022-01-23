193832dea2c90dacc6966db8c2597646164f4c61
Nils Streedain <25465133+nilsstreedain@users.noreply.github.com>
2022-01-23 00:47:58 -0800

    Implemented all of prog2 but fib loop

--- prog2.asm
+++ prog2.asm
@@ -1,17 +1,17 @@
 TITLE Program Template     (prog2.asm)
 
 ; Author:					Nils Streedain
-; Last Modified:			1/16/2022
+; Last Modified:			1/22/2022
 ; OSU email address:		streedan@oregonstate.edu
 ; Course number/section:	271-001
-; Assignment Number:		1
-; Due Date:					1/16/2022
-; Description:				Write a MASM program to perform the tasks listed below. Test your program to ensure that it functions correctly.
-;							Display your name and program title on the output screen.
-;							Display instructions for the user.
-;							Prompt the user to enter two numbers.
-;							Calculate the sum, difference, product, (integer) quotient and remainder of the numbers.
-;							Display a terminating message.
+; Assignment Number:		2
+; Due Date:					1/22/2022
+; Description:				Write a program to calculate Fibonacci numbers.
+;							Display the program title and programmer’s name. Then prompt the user for their name and greet them (by name).
+;							Prompt the user to enter the number of Fibonacci terms to be displayed. Advise the user to enter an integer in the range [1 .. 46].
+;							Get and validate the user input (n).
+;							Calculate and display all of the Fibonacci numbers up to and including the nth term. The results should be displayed 4 terms per line with at least 5 spaces between terms.
+;							Display a parting message that includes the user’s name, and terminate the program.
 
 INCLUDE Irvine32.inc
 
@@ -20,232 +20,89 @@ INCLUDE Irvine32.inc
 .data
 ; (insert variable definitions here)
 	; interface strings
-	prog_title	BYTE		"Elementary Arithmetic by Nils Streedain", 13, 10, 0
-	extra_1		BYTE		"**EC: Program verifies second number less than first.", 13, 10, 0
-	extra_2		BYTE		"**EC: Program displays the square offset each number.", 13, 10, 0
-	intro		BYTE		"Enter 2 numbers, and I'll show you the sum, difference, product, quotient, and remainder.", 13, 10, 0
-	prompt_1	BYTE		"First number: ", 0
-	prompt_2	BYTE		"Second number: ", 0
-	error_1		BYTE		"The second number must be less than the first!", 13, 10, 0
-	bye			BYTE		"Impressed? Bye!", 0
+	prog_title	BYTE		"Fibonacci Numbers", 13, 10, "Programmed by Nils Streedain", 13, 10, 0
+	; extra_1		BYTE		"**EC: Displays the numbers in aligned columns.", 13, 10, 0
+	prompt_1	BYTE		"What is your name? ", 0
+	greeting	BYTE		"Hello, ", 0
+	prompt_2a	BYTE		"Enter the number of Fibonacci terms to be displayed.", 13, 10, "Provide the number as an integer in the range [1 .. 46].", 13, 10, 0
+	prompt_2b	BYTE		"How many Fibonacci terms do you want? ", 0
+
+	error		BYTE		"Out of range. Enter a number in [1 .. 46]", 13, 10, 0
+	bye		BYTE		"Results certified by Nils Streedain.", 13, 10, "Goodbye, ", 0
 
 	; user inputs
-	input_1		DWORD		?
-	input_2		DWORD		?
+	username	BYTE		33 DUP(0)
+	num_fibs	DWORD		?
 
-	; equation formatting strings
-	add_char	BYTE		" + ", 0
-	sub_char	BYTE		" - ", 0
-	mul_char	BYTE		" x ", 0
-	div_char	BYTE		" / ", 0
-	eql_char	BYTE		" = ", 0
-	rem_string	BYTE		" remainder ", 0
-	squ_string	BYTE		"Square of ", 0
-
-	; calculated values
-	sum			DWORD		?
-	difference	DWORD		?
-	product		DWORD		?
-	quotient	DWORD		?
-	remainder	DWORD		?
-	square_1	DWORD		?
-	square_2	DWORD		?
+	; program values
+	range_min	DWORD		1
+	range_max	DWORD		46
+	curr_num	DWORD		?
 
 .code
 main PROC
 
-; (insert executable instructions here)
-
-; 1. Introduction
-	; print title
+; Prints the program title & author's name.
+introduction:
 	mov		edx, OFFSET prog_title
 	call	WriteString
 
-	; print extra credit
-	mov		edx, OFFSET extra_1
-	call	WriteString
-	mov		edx, OFFSET extra_2
-	call	WriteString
-
-	; print intro
-	mov		edx, OFFSET intro
-	call	WriteString
-
-; 2. Get the data
-	; ask for and save first input number
+; Asks for and stores user's name. Also greets user using the name.
+getUserInfo:
+	; prompt for username
 	mov		edx, OFFSET prompt_1
 	call	WriteString
+
+	; store username
+	mov		edx, OFFSET username
+	mov		ecx, 32
+	call	ReadString
+
+	; greet user
+	mov		edx, OFFSET greeting
+	call	WriteString
+	mov		edx, OFFSET username
+	call	WriteString
+	call	Crlf
+
+; Tells the user to enter the number of fibs they'd like printed.
+displayinstructions:
+	mov		edx, OFFSET prompt_2a
+	call	WriteString
+
+; Prompts for number of fibs in the range 1-46. If the input is out of range, the program jumps to outOfRange.
+fibPrompt:
+	; prompts for number of fibs in range
+	mov		edx, OFFSET prompt_2b
+	call	WriteString
 	call	ReadInt
-	mov		input_1, eax
 
-	; ask for and save second input number
-	mov		edx, OFFSET prompt_2
-	call	WriteString
-	call	ReadInt
-	mov		input_2, eax
-
-	; make sure input_1 > input_2
-	mov		eax, input_1
-	cmp		eax, input_2
-	jle		Error
-	jg		Calculate
-
-; run if input_2 >= input_1
-Error:
-	; prints error and ends program
-	mov		edx, OFFSET error_1
-	call	WriteString
-	jmp		Farewell
-
-; 3. Calculate the required values
-Calculate:
-	; calculate sum
-	mov		eax, input_1
-	add		eax, input_2
-	mov		sum, eax
-
-	; calculate difference
-	mov		eax, input_1
-	sub		eax, input_2
-	mov		difference, eax
-
-	; calculate product
-	mov		eax, input_1
-	mov		ebx, input_2
-	mul		ebx
-	mov		product, eax
-
-	; caclulate quotient
-	mov		eax, input_1
-	mov		ebx, input_2
-	div		ebx
-	mov		quotient, eax
-	mov		remainder, edx
-
-	; calculate square 1
-	mov		eax, input_1
-	mov		ebx, input_1
-	mul		ebx
-	mov		square_1, eax
-
-	; calculate square 2
-	mov		eax, input_2
-	mov		ebx, input_2
-	mul		ebx
-	mov		square_2, eax
-
-; 4. Display the results
-	; display sum "input_1 + input_2 = sum"
-	mov		eax, input_1
-	call	WriteDec
-
-	mov		edx, OFFSET add_char
-	call	WriteString
-
-	mov		eax, input_2
-	call	WriteDec
-
-	mov		edx, OFFSET eql_char
-	call	WriteString
+	; check if input is out of range
+	; (ReadInt already stores input in eax)
+	cmp		eax, range_min
+	jl		outOfRange
+	cmp		eax, range_max
+	jg		outOfRange
 	
-	mov		eax, sum
-	call	WriteDec
+	; if not outOfRange, store num_fibs & then displayFibs
+	mov		num_fibs, eax
+	jmp		displayFibs
 
-	call	Crlf
-
-	; display difference  "input_1 - input_2 = difference"
-	mov		eax, input_1
-	call	WriteDec
-
-	mov		edx, OFFSET sub_char
+; Gives the user an out of range error & then jumps to fibPrompt to get another user input.
+outOfRange:
+	mov		edx, OFFSET error
 	call	WriteString
+	jmp		fibPrompt
 
-	mov		eax, input_2
-	call	WriteDec
+; 4. Display Fibs
+displayFibs:
 
-	mov		edx, OFFSET eql_char
-	call	WriteString
-	
-	mov		eax, difference
-	call	WriteDec
 
-	call	Crlf
-
-	; display product  "input_1 x input_2 = product"
-	mov		eax, input_1
-	call	WriteDec
-
-	mov		edx, OFFSET mul_char
-	call	WriteString
-
-	mov		eax, input_2
-	call	WriteDec
-
-	mov		edx, OFFSET eql_char
-	call	WriteString
-	
-	mov		eax, product
-	call	WriteDec
-
-	call	Crlf
-
-	; display quotient  "input_1 / input_2 = quotient remainder remainder"
-	mov		eax, input_1
-	call	WriteDec
-
-	mov		edx, OFFSET div_char
-	call	WriteString
-
-	mov		eax, input_2
-	call	WriteDec
-
-	mov		edx, OFFSET eql_char
-	call	WriteString
-	
-	mov		eax, quotient
-	call	WriteDec
-
-	mov		edx, OFFSET rem_string
-	call	WriteString
-	
-	mov		eax, remainder
-	call	WriteDec
-
-	call	Crlf
-
-	; display square 1 "Square of input_1 = square_1"
-	mov		edx, OFFSET squ_string
-	call	WriteString
-
-	mov		eax, input_1
-	call	WriteDec
-
-	mov		edx, OFFSET eql_char
-	call	WriteString
-
-	mov		eax, square_1
-	call	WriteDec
-
-	call	Crlf
-
-	; display square 2 "Square of input_2 = square_2"
-	mov		edx, OFFSET squ_string
-	call	WriteString
-
-	mov		eax, input_2
-	call	WriteDec
-
-	mov		edx, OFFSET eql_char
-	call	WriteString
-
-	mov		eax, square_2
-	call	WriteDec
-
-	call	Crlf
-
-; 5. Say goodbye
-Farewell:
+; Gives the user a salutation & exits the program.
+goodbye:
 	mov		edx, OFFSET bye
+	call	WriteString
+	mov		edx, OFFSET username
 	call	WriteString
 
 	exit	; exit to operating system
